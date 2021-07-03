23ea59b83619379fe4327f0f4b1ce1bbd5a12d57
J <73671839+JAOS-DEV@users.noreply.github.com>
2021-07-03 19:59:10 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -7,10 +7,22 @@ const scissorsIconBtnEl = document.querySelector(".img-icon-scissors");
 const paperImgEl = document.getElementById("paperImg");
 const rockImgEl = document.getElementById("rockimg");
 const scissorsImgEl = document.getElementById("scissorsImg");
+const placeholderBtnEl = document.getElementById("placeholderBtn");
+const iconContainer = document.querySelector(".icon-choice");
+const pickedTextEl = document.querySelectorAll(".pickedText");
+const winnerTextEL = document.querySelector(".winner-text");
+const playAgainEL = document.querySelector(".play-again");
 
 // ================================================
-
+let score = 0;
 // ================================================
+const init = () => {
+  score = 0;
+  pickedTextEl.classList.add("hidden");
+  // Change paper to selcted icon
+  paperIconBtnEl.classList.add(`img-icon-paper`);
+  paperIconBtnEl.classList.remove(`img-icon-${id}`);
+};
 
 // Random icon
 const randomIcon = () => {
@@ -33,8 +45,8 @@ iconsEl.forEach((iconEl) => {
   });
 });
 
-// ===================
-
+// ===================================
+// Display chosen icons
 const displayChosenIcon = (buttonEl, imageEl) => {
   console.log("Display chosen Icon + random icon");
   const { id } = buttonEl;
@@ -42,38 +54,138 @@ const displayChosenIcon = (buttonEl, imageEl) => {
   const computerIcon = randomIcon();
 
   // displayChosenIcons
-
+  // Show picked text
+  pickedTextEl.forEach((el) => el.classList.remove("hidden"));
   // Hide rock Icon
   rockIconBtnEl.classList.add("hidden");
   // Change paper to selcted icon
   paperIconBtnEl.classList.remove(`img-icon-paper`);
   paperIconBtnEl.classList.add(`img-icon-${id}`);
   paperImg.src = src;
-  // Change scissors to computers icon
+
+  // Change scissors to computers icon after delay
+  placeholderBtnEl.classList.remove("hidden");
+  scissorsIconBtnEl.classList.add("hidden");
+  setTimeout(() => {
+    showHouseIcon(id, computerIcon);
+  }, 3000);
+
+  // Css to fix icon alignment
+  iconContainer.classList.add("icon-choice-flex");
+  paperIconBtnEl.style.margin = "0rem";
+  scissorsIconBtnEl.style.margin = "0rem";
+};
+
+const showHouseIcon = (id, computerIcon) => {
+  placeholderBtnEl.classList.add("hidden");
+  scissorsIconBtnEl.classList.remove("hidden");
   scissorsIconBtnEl.classList.remove(`img-icon-scissors`);
   scissorsIconBtnEl.classList.add(`img-icon-${computerIcon}`);
   scissorsImg.src = `./images/icon-${computerIcon}.svg`;
-
   // Check winner
   checkWinner(id, computerIcon);
-  // ==============
 };
 
 // Check Winner
 const checkWinner = (id, computerIcon) => {
-  if (id === computerIcon) return console.log(`draw`);
+  if (id === computerIcon) {
+    console.log(`draw`);
+    // Show play again button
+    playAgainEL.classList.remove("hidden");
+    // Change winner text to "DRAW"
+    winnerTextEL.textContent = "DRAW";
+    // Update scoreboard
+    console.log(score);
+    // Display winner text
+    winnerTextEL.classList.remove("hidden");
+  }
   // Paper Logic
-  if (id === "paper" && computerIcon === "rock") return console.log("winner");
-
-  if (id === "paper" && computerIcon === "scissors") return console.log("loser");
+  if (id === "paper" && computerIcon === "rock") {
+    console.log("winner");
+    // Show play again button
+    playAgainEL.classList.remove("hidden");
+    // Change winner text to "DRAW"
+    winnerTextEL.textContent = "WINNER!";
+    // Update scoreboard
+    score++;
+    console.log(score);
+    // Display winner text
+    winnerTextEL.classList.remove("hidden");
+  }
+  if (id === "paper" && computerIcon === "scissors") {
+    console.log("loser");
+    // Show play again button
+    playAgainEL.classList.remove("hidden");
+    // Change winner text to "DRAW"
+    winnerTextEL.textContent = "LOSER!";
+    // Update scoreboard
+    score--;
+    console.log(score);
+    // Display winner text
+    winnerTextEL.classList.remove("hidden");
+  }
 
   // Rock Logic
-  if (id === "rock" && computerIcon === "scissors") return console.log("winner");
+  if (id === "rock" && computerIcon === "scissors") {
+    console.log("winner");
+    // Show play again button
+    playAgainEL.classList.remove("hidden");
+    // Change winner text to "DRAW"
+    winnerTextEL.textContent = "WINNER!";
+    // Update scoreboard
+    score++;
+    console.log(score);
+    // Display winner text
+    winnerTextEL.classList.remove("hidden");
+  }
 
-  if (id === "rock" && computerIcon === "paper") return console.log("loser");
+  if (id === "rock" && computerIcon === "paper") {
+    console.log("loser");
+    // Show play again button
+    playAgainEL.classList.remove("hidden");
+    // Change winner text to "DRAW"
+    winnerTextEL.textContent = "LOSER!";
+    // Update scoreboard
+    score--;
+    console.log(score);
+    // Display winner text
+    winnerTextEL.classList.remove("hidden");
+  }
 
   // Scissors Logic
-  if (id === "scissors" && computerIcon === "paper") return console.log("winner");
+  if (id === "scissors" && computerIcon === "paper") {
+    console.log("winner");
+    // Show play again button
+    playAgainEL.classList.remove("hidden");
+    // Change winner text to "DRAW"
+    winnerTextEL.textContent = "WINNER!";
+    // Update scoreboard
+    score++;
+    console.log(score);
+    // Display winner text
+    winnerTextEL.classList.remove("hidden");
+  }
 
-  if (id === "scissors" && computerIcon === "rock") return console.log("loser");
+  if (id === "scissors" && computerIcon === "rock") {
+    console.log("loser");
+    // Show play again button
+    playAgainEL.classList.remove("hidden");
+    // Change winner text to "DRAW"
+    winnerTextEL.textContent = "LOSER!";
+    // Update scoreboard
+    score--;
+    console.log(score);
+    // Display winner text
+    winnerTextEL.classList.remove("hidden");
+  }
+  console.log(`score end ${score}`);
+  paperIconBtnEl.classList.add("img-icon-radial-effect");
 };
+
+// ======================================
+// Play again button
+const playAgain = () => {};
+
+playAgainEL.addEventListener("click", (e) => {
+  playAgain();
+});
